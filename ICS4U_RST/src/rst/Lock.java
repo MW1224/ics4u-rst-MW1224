4168cc4cda123091d5f906fe05f385d00caadf11
mayra <mayra@192.168.8.145>
2024-01-05 16:38:05 -0500

    Finished lock class (except for comments,
    Also added lockNumber data field to hold lock's number out of all locks

--- ICS4U_RST/src/rst/Lock.java
+++ ICS4U_RST/src/rst/Lock.java
@@ -3,15 +3,65 @@ package rst;
 public class Lock {
 	private String clue, hint;
 	private boolean state;	// false if locked, true if unlocked
-	private int combo, hintCount;
+	private int lockNumber, combo, hintCount;
 	
-	public Lock(String lockClue, String lockHint, int lockCombo) {
+	public Lock(String lockClue, String lockHint, int lockCombo, int lockNum) {
 		clue = lockClue;
 		hint =lockHint;
 		state = false;
 		combo = lockCombo;
+		lockNumber = lockNum;
 		hintCount = 0;
 	}
 	
+	public boolean unlock(int comboAttempt) {
+		if (comboAttempt == combo) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
+	public boolean isOpen() {
+		return state;
+	}
+	
+	public int getCombo() {
+		return combo;
+	}
+	
+	public String getClue() {
+		return clue;
+	}
+	
+	public String getHint() {
+		if (hintCount == 0) {
+			hintCount++;
+			return "Hint (-$3): " + hint;
+		} else {
+			return "Already got hint. (" + hint + ")";
+		}
+	}
+	
+	public int getLockNumber() {
+		return lockNumber;
+	}
+	
+	public boolean equals(Object obj) {
+		Lock testLock = (Lock) obj;
+		if (testLock.getLockNumber() == lockNumber && testLock.getCombo() == combo && testLock.getClue().equals(clue) && testLock.getHint().equals(hint)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public String toString() {
+		String lockString = "Lock " + lockNumber + " (";
+		
+		int numDigits = String.valueOf(combo).length();
+		lockString += numDigits + "-digit combo)";
+		
+		return lockString;
+	}
 }
