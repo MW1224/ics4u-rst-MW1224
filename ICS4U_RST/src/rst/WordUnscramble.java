e18943de3c3829315de299818128edd98c338b36
mayra <mayra@192.168.8.145>
2024-01-07 15:35:00 -0500

    Finished WordUnscramble (except for comments)

--- ICS4U_RST/src/rst/WordUnscramble.java
+++ ICS4U_RST/src/rst/WordUnscramble.java
@@ -35,4 +35,39 @@ public class WordUnscramble extends MiniGame {
 		return "Bonus money (starting at $" + START_BONUS + "):\n\t- Each incorrect word: -$" + (-1 * INCORRECT_FEE)
 				+ "\n\t- Each correct word: +$" + CORRECT_BONUS + "\n\t- Each blank word: -$" + BLANK_FEE;
 	}
+	
+	public String[] checkWords(String[] wordsToCheck) {
+		String[] correctWords = new String[22];
+		boolean foundMatch = false;
+		
+		// Check words entered by player
+		for (int i = 0; i < wordsToCheck.length; i++) {
+			String testWord = wordsToCheck[i].toLowerCase();
+			
+			if (testWord.isEmpty()) {	// if word is blank
+				correctWords[i] = BLANK;
+				bonusMoney += BLANK_FEE;
+			} else {
+				for (String possibleWord : POSSIBLE_WORDS) {
+					if (testWord.equals(possibleWord)) {
+						foundMatch = true;
+					}
+				}
+				
+				if (foundMatch) {
+					correctWords[i] = CORRECT;
+					bonusMoney += CORRECT_BONUS;
+					possibleWordsLeft.remove(testWord);
+				} else {
+					correctWords[i] = INCORRECT;
+					bonusMoney += INCORRECT_FEE;
+				}
+			}
+		}
+		return correctWords;
+	}
+	
+	public String toString() {
+		return "Word Unscramble";
+	}
 }
