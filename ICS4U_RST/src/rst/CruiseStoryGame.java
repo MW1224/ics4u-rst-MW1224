7e1538fe621d42abe6dc493d9a19ec775250b747
mayra <mayra@192.168.8.145>
2024-01-07 22:43:41 -0500

    Added JavaFX application, but didn't actually write code inside them
    Only put class documentation with name, course, etc... & launch(args) in
    main method

---
+++ ICS4U_RST/src/rst/CruiseStoryGame.java
@@ -0,0 +1,27 @@
+package rst;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Mayra Wang
+ * Date: January 2024
+ * Course: ICS4U
+ * CruiseStoryGame.java
+ * RST
+ */
+public class CruiseStoryGame extends Application {
+	
+	@Override
+	public void stop() throws Exception {
+		
+	}
+	
+	public void start(Stage myStage) throws Exception {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
