865e21416a0cafd5a1e0180e085a6fe8e02be1ec
mayra <mayra@10.78.25.154>
2024-01-08 14:07:12 -0500

    Currently on TilePane in initial main screen when program starts
    Finished title Label, VBox root node (layout) and some global constants
    for layout uses

--- ICS4U_RST/src/rst/CruiseStoryGame.java
+++ ICS4U_RST/src/rst/CruiseStoryGame.java
@@ -1,6 +1,13 @@
 package rst;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -12,12 +19,46 @@ import javafx.stage.Stage;
  */
 public class CruiseStoryGame extends Application {
 	
+	// Global constants
+	private static final int GAP = 10;
+	private static final int SMALL_FONT = 14, MEDIUM_FONT = 20, LARGE_FONT = 26;
+	
+	// References for objects
+	Passenger passenger;
+	
+	/**
+	 * Overridden method to handle what happens when application stops.
+	 * Saves high score to text file.
+	 */
 	@Override
 	public void stop() throws Exception {
-		
+		FileHandler.saveToFile(passenger.getHighScore());
 	}
 	
+	/**
+	 * Overridden method to handle what happens when the application starts.
+	 */
+	@Override
 	public void start(Stage myStage) throws Exception {
+		// Local constants
+		final int NUM_OF_TILE_COLS = 2;
+		
+		// Set layout
+		VBox root = new VBox(GAP);
+		root.setPadding(new Insets(GAP, GAP, GAP, GAP));
+		
+		// Add title label
+		Label lblTitle = new Label("Welcome to the cruise story game!");
+		lblTitle.setFont(Font.font(LARGE_FONT));
+		root.getChildren().add(lblTitle);
+		
+		// Add TilePane layout
+		TilePane tileInstructions = new TilePane();
+		tileInstructions.setPrefColumns(NUM_OF_TILE_COLS);
+		// Add ImageView to hold image of Royal Caribbean's logo
+		ImageView imgLogo = new ImageView(new Image(getClass().getResource("/images/logo.png").toString()));
+		tileInstructions.getChildren().add(imgLogo);
+		// Add Label to hold game instructions
 		
 	}
 
