10de8253adaa4f14e852c958179f14abc42a0823
mayra <mayra@10.78.25.154>
2024-01-09 14:09:08 -0500

    Currently adding Images to main screen in TilePane

--- ICS4U_RST/src/rst/CruiseStoryGame.java
+++ ICS4U_RST/src/rst/CruiseStoryGame.java
@@ -2,6 +2,8 @@ package rst;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -30,10 +32,10 @@ public class CruiseStoryGame extends Application {
 	 * Overridden method to handle what happens when application stops.
 	 * Saves high score to text file.
 	 */
-	@Override
+	/*@Override
 	public void stop() throws Exception {
 		FileHandler.saveToFile(passenger.getHighScore());
-	}
+	}*/
 	
 	/**
 	 * Overridden method to handle what happens when the application starts.
@@ -41,25 +43,47 @@ public class CruiseStoryGame extends Application {
 	@Override
 	public void start(Stage myStage) throws Exception {
 		// Local constants
-		final int NUM_OF_TILE_COLS = 2;
+		final int NUM_OF_TILE_COLS = 4;
 		
 		// Set layout
 		VBox root = new VBox(GAP);
 		root.setPadding(new Insets(GAP, GAP, GAP, GAP));
+		root.setAlignment(Pos.CENTER);
 		
-		// Add title label
+		// Add title Label
 		Label lblTitle = new Label("Welcome to the cruise story game!");
 		lblTitle.setFont(Font.font(LARGE_FONT));
 		root.getChildren().add(lblTitle);
 		
-		// Add TilePane layout
+		// Add ImageView holding Royal Caribbean's logo
+		ImageView imgLogo = new ImageView(new Image(getClass().getResource("/images/logo.png").toString()));
+		root.getChildren().add(imgLogo);
+		
+		// TilePane layout
 		TilePane tileInstructions = new TilePane();
 		tileInstructions.setPrefColumns(NUM_OF_TILE_COLS);
-		// Add ImageView to hold image of Royal Caribbean's logo
-		ImageView imgLogo = new ImageView(new Image(getClass().getResource("/images/logo.png").toString()));
-		tileInstructions.getChildren().add(imgLogo);
-		// Add Label to hold game instructions
+		tileInstructions.setAlignment(Pos.CENTER);
+		// ImageViews to hold image of Oasis of the Seas (cruise ship's name) & its aqua theatre
+		ImageView imgOasis = new ImageView(new Image(getClass().getResource("/images/oasis.png").toString()));
+		ImageView imgAquaTheatre = new ImageView(new Image(getClass().getResource("/images/aquaTheatre.png").toString()));
+		// Label to hold the game's intro & overall instructions
+		Label lblIntro = new Label(Passenger.showGameIntro());
+		lblIntro.setFont(Font.font(MEDIUM_FONT));
+		// Label to hold game's goal information
+		Label lblGoal = new Label(Passenger.showGameGoal());
+		lblGoal.setFont(Font.font(MEDIUM_FONT));
+		// ImageViews to hold images of Oasis OTS's Central Park
+		ImageView imgCentralPark = new ImageView(new Image(getClass().getResource("/images/centralPark.png").toString()));
+		ImageView imgCentralPark2 = new ImageView(new Image(getClass().getResource("/images/centralPark2.png").toString()));
+		tileInstructions.getChildren().addAll(imgOasis, imgAquaTheatre, lblIntro, lblGoal, imgCentralPark, imgCentralPark2);
+		root.getChildren().add(tileInstructions);
 		
+		// Create scene with root VBox
+		Scene mainScene = new Scene(root);
+		
+		myStage.setTitle("Cruise Story Game");
+		myStage.setScene(mainScene);
+		myStage.show();
 	}
 
 	public static void main(String[] args) {
