1ecedb3b28d1d636a1521deb52047e9567cbca0c
mayra <mayra@10.78.25.154>
2024-01-11 14:06:24 -0500

    Currently on scene 2 (Cruise Route Selection)- done top & centre of pane

--- ICS4U_RST/src/rst/CruiseStoryGame.java
+++ ICS4U_RST/src/rst/CruiseStoryGame.java
@@ -1,6 +1,7 @@
 package rst;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -10,11 +11,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 /**
  * @author Mayra Wang
@@ -27,19 +31,25 @@ public class CruiseStoryGame extends Application {
 	
 	// Global constants
 	private static final int GAP = 10;
-	private static final int SMALL_FONT = 15, MEDIUM_FONT = 20, LARGE_FONT = 26;
+	private static final int SMALL_FONT = 15, MEDIUM_FONT = 17, LARGE_FONT = 20, XL_FONT = 26;
 	
 	// References for objects
 	Passenger passenger;
+	
+	// Input/output UI controls for main screen
+	private Label lblHighScore, lblErrorMessage;
+	private TextField txtFirstName, txtLastName, txtEmail, txtPhoneNumber;
+	private ChoiceBox<String> chcLoyaltyStatus;
+	private Scene mainScene;
 	
 	/**
 	 * Overridden method to handle what happens when application stops.
 	 * Saves high score to text file.
 	 */
-	/*@Override
+	@Override
 	public void stop() throws Exception {
 		FileHandler.saveToFile(passenger.getHighScore());
-	}*/
+	}
 	
 	/**
 	 * Overridden method to handle what happens when the application starts.
@@ -47,7 +57,9 @@ public class CruiseStoryGame extends Application {
 	@Override
 	public void start(Stage myStage) throws Exception {
 		// Local constants
-		
+		final int TILE_HEIGHT = 185, TILE_WIDTH = 200;
+		final int IMG_HEIGHT = 175;
+		final int SCREEN_WIDTH = 1100, SCREEN_HEIGHT = 900;
 		
 		// Set layout
 		VBox root = new VBox(GAP);
@@ -60,7 +72,7 @@ public class CruiseStoryGame extends Application {
 		
 		// Add title Label
 		Label lblTitle = new Label("Welcome to the cruise story game!");
-		lblTitle.setFont(Font.font(LARGE_FONT));
+		lblTitle.setFont(Font.font(XL_FONT));
 		root.getChildren().add(lblTitle);
 		
 		// Add ImageView holding Royal Caribbean's logo
@@ -68,40 +80,40 @@ public class CruiseStoryGame extends Application {
 		root.getChildren().add(imgLogo);
 		
 		// TilePane layout
-		TilePane tileInstructions = new TilePane(10, 0);
+		TilePane tileInstructions = new TilePane(GAP, 0);
 		tileInstructions.setAlignment(Pos.CENTER);
 		
 		// ImageViews to hold images of Oasis of the Seas
 		ImageView imgOasis = new ImageView(new Image(getClass().getResource("/images/oasis.png").toString()));
-		imgOasis.setFitHeight(200);
+		imgOasis.setFitHeight(IMG_HEIGHT);
 		imgOasis.setPreserveRatio(true);
 		
 		ImageView imgAquaTheatre = new ImageView(new Image(getClass().getResource("/images/aquaTheatre.png").toString()));
-		imgAquaTheatre.setFitHeight(200);
+		imgAquaTheatre.setFitHeight(IMG_HEIGHT);
 		imgAquaTheatre.setPreserveRatio(true);
 		
 		ImageView imgOasis2 = new ImageView(new Image(getClass().getResource("/images/oasis2.png").toString()));
-		imgOasis2.setFitHeight(200);
+		imgOasis2.setFitHeight(IMG_HEIGHT);
 		imgOasis2.setPreserveRatio(true);
 		
 		ImageView imgBoardwalk = new ImageView(new Image(getClass().getResource("/images/boardwalk.png").toString()));
-		imgBoardwalk.setFitHeight(200);
+		imgBoardwalk.setFitHeight(IMG_HEIGHT);
 		imgBoardwalk.setPreserveRatio(true);
 		
 		ImageView imgOasisBack = new ImageView(new Image(getClass().getResource("/images/oasisBack.png").toString()));
-		imgOasisBack.setFitHeight(200);
+		imgOasisBack.setFitHeight(IMG_HEIGHT);
 		imgOasisBack.setPreserveRatio(true);
 		
 		ImageView imgCentralPark = new ImageView(new Image(getClass().getResource("/images/centralPark.png").toString()));
-		imgCentralPark.setFitHeight(200);
+		imgCentralPark.setFitHeight(IMG_HEIGHT);
 		imgCentralPark.setPreserveRatio(true);
 		
 		ImageView imgCentralPark2 = new ImageView(new Image(getClass().getResource("/images/centralPark2.png").toString()));
-		imgCentralPark2.setFitHeight(200);
+		imgCentralPark2.setFitHeight(IMG_HEIGHT);
 		imgCentralPark2.setPreserveRatio(true);
 		
 		ImageView imgOasisTop = new ImageView(new Image(getClass().getResource("/images/oasisTop.png").toString()));
-		imgOasisTop.setFitHeight(200);
+		imgOasisTop.setFitHeight(IMG_HEIGHT);
 		imgOasisTop.setPreserveRatio(true);
 		
 		// Label to hold the game's intro & overall instructions
@@ -113,10 +125,10 @@ public class CruiseStoryGame extends Application {
 		lblGoal.setFont(Font.font(MEDIUM_FONT));
 		lblGoal.setWrapText(true);
 		tileInstructions.getChildren().addAll(imgOasis, imgAquaTheatre, imgOasis2, lblIntro, imgBoardwalk, imgOasisBack, lblGoal, imgCentralPark, imgCentralPark2, imgOasisTop);
-		tileInstructions.setPrefTileHeight(210);
-		tileInstructions.setPrefTileWidth(240);
-		tileInstructions.setPrefHeight(420);
-		tileInstructions.setPrefWidth(1200);
+		tileInstructions.setPrefTileHeight(TILE_HEIGHT);
+		tileInstructions.setPrefTileWidth(TILE_WIDTH);
+		tileInstructions.setPrefHeight(TILE_HEIGHT * 2);
+		tileInstructions.setPrefWidth(TILE_WIDTH * 5);
 		root.getChildren().add(tileInstructions);	// Add this TilePane to the Vbox root
 		
 		// GridPane to obtain input for passenger's information
@@ -126,48 +138,48 @@ public class CruiseStoryGame extends Application {
 		
 		// Label for instructions for passenger to enter their information
 		Label lblPassengerInfo = new Label(Passenger.showPassengerInfoPrompt());
-		lblPassengerInfo.setFont(Font.font(MEDIUM_FONT));
+		lblPassengerInfo.setFont(Font.font(LARGE_FONT));
 		gridPassengerInfo.add(lblPassengerInfo, 3, 0, 4, 1); // pos (3,0), colspan = 4, rowspan = 1
 		
 		// Label for passenger's first name
 		Label lblFirstName = new Label("First name:");
-		lblFirstName.setFont(Font.font(SMALL_FONT));
+		lblFirstName.setFont(Font.font(MEDIUM_FONT));
 		gridPassengerInfo.add(lblFirstName, 0, 1, 2, 1);	// pos (0,1), colspan = 2, rowspan = 1
 		// TextField for passenger's first name
-		TextField txtFirstName = new TextField();
+		txtFirstName = new TextField();
 		gridPassengerInfo.add(txtFirstName, 2, 1, 2, 1);	// pos (2,1), colspan = 2, rowspan = 1
 		// Label for passenger's last name
 		Label lblLastName = new Label("Last name:");
-		lblLastName.setFont(Font.font(SMALL_FONT));
+		lblLastName.setFont(Font.font(MEDIUM_FONT));
 		gridPassengerInfo.add(lblLastName, 6, 1, 2, 1);	// pos (6,1), colspan = 2, rowspan = 1
 		// TextField for passenger's first name
-		TextField txtLastName = new TextField();
+		txtLastName = new TextField();
 		gridPassengerInfo.add(txtLastName, 8, 1, 2, 1);	// pos (8,1), colspan = 2, rowspan = 1
 		// Label for passenger's email
 		Label lblEmail = new Label("Email:");
-		lblEmail.setFont(Font.font(SMALL_FONT));
+		lblEmail.setFont(Font.font(MEDIUM_FONT));
 		gridPassengerInfo.add(lblEmail, 1, 2);	// pos (1,2), colspan = 1, rowspan = 1
 		// TextField for passenger's email
-		TextField txtEmail = new TextField();
+		txtEmail = new TextField();
 		gridPassengerInfo.add(txtEmail, 2, 2, 2, 1);	// pos (2,2), colspan = 2, rowspan = 1
 		// Label for passenger's phone number
 		Label lblPhoneNumber = new Label("Phone number:");
-		lblPhoneNumber.setFont(Font.font(SMALL_FONT));
+		lblPhoneNumber.setFont(Font.font(MEDIUM_FONT));
 		gridPassengerInfo.add(lblPhoneNumber, 6, 2, 2, 1);	// pos (6,2), colspan = 2, rowspan = 1
 		// TextField for passenger's phone number
-		TextField txtPhoneNumber = new TextField();
+		txtPhoneNumber = new TextField();
 		gridPassengerInfo.add(txtPhoneNumber, 8, 2, 2, 1);	// pos (8,2), colspan = 2, rowspan = 1
 		// Label for passenger's loyalty status
 		Label lblLoyaltyStatus = new Label("Crown & Anchor Society status:");
-		lblLoyaltyStatus.setFont(Font.font(SMALL_FONT));
+		lblLoyaltyStatus.setFont(Font.font(MEDIUM_FONT));
 		gridPassengerInfo.add(lblLoyaltyStatus, 1, 3, 2, 1);	// pos (1,3), colspan = 2, rowspan = 1
 		// ChoiceBox for loyalty status options
-		ChoiceBox<String> chcLoyaltyStatus = new ChoiceBox<String>();
+		chcLoyaltyStatus = new ChoiceBox<String>();
 		chcLoyaltyStatus.getItems().addAll(Passenger.LOYALTY_STATUSES);
 		gridPassengerInfo.add(chcLoyaltyStatus, 3, 3, 2, 1);	// pos (3,3), colspan = 2, rowspan = 1
 		// Label to display overall high score between all games played before
-		Label lblHighScore = new Label(passenger.showHighScore());
-		lblHighScore.setFont(Font.font(SMALL_FONT));
+		lblHighScore = new Label(passenger.showHighScore());
+		lblHighScore.setFont(Font.font(MEDIUM_FONT));
 		gridPassengerInfo.add(lblHighScore, 1, 4, 2, 1);	// pos (1,4), colspan = 2, rowspan = 1
 		// Button for event handler to reset high score out of all games played to $0
 		Button btnResetHighScore = new Button("Reset high score counter");
@@ -178,12 +190,18 @@ public class CruiseStoryGame extends Application {
 		Button btnFinish = new Button("Done entering info");
 		btnFinish.setFont(Font.font(SMALL_FONT));
 		gridPassengerInfo.add(btnFinish, 8, 4, 2, 1);	// pos (8,4), colspan = 1, rowspan = 1
-		// Add this GridPane to the Vbox root
+		btnFinish.setOnAction(event -> addPassengerInfo());
+		// Add this GridPane to the VBox root
 		gridPassengerInfo.setAlignment(Pos.CENTER);
 		root.getChildren().add(gridPassengerInfo);
 		
+		// Add label in case of error message to bottom of VBox root
+		lblErrorMessage = new Label("");
+		lblErrorMessage.setFont(Font.font(SMALL_FONT));
+		root.getChildren().add(lblErrorMessage);
+		
 		// Create scene with root VBox
-		Scene mainScene = new Scene(root, 1300, 900);
+		mainScene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT);
 		
 		myStage.setTitle("Cruise Story Game");
 		myStage.setScene(mainScene);
@@ -191,7 +209,99 @@ public class CruiseStoryGame extends Application {
 	}
 	
 	private void resetHighScore() {
+		passenger.resetHighScore();
+		lblHighScore.setText(passenger.showHighScore());
+	}
+	
+	private void addPassengerInfo() {
+		String firstName, lastName, email, phoneNumber, loyaltyStatus;
+		firstName = txtFirstName.getText().trim();
+		lastName = txtLastName.getText().trim();
+		email = txtEmail.getText().trim();
+		phoneNumber = txtLastName.getText().trim();
+		loyaltyStatus = chcLoyaltyStatus.getValue();
 		
+		if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || phoneNumber.isEmpty() || loyaltyStatus == null) {
+			lblErrorMessage.setText("Please enter information in all fields! (no empty boxes)");
+			return;
+		}
+		
+		passenger.setFirstName(firstName);
+		passenger.setLastName(lastName);
+		passenger.setEmail(email);
+		passenger.setPhoneNumber(phoneNumber);
+		passenger.setLoyaltyStatus(loyaltyStatus);
+		
+		showCruiseRouteScreen();
+	}
+	
+	private void showCruiseRouteScreen() {
+		// Local constants
+		final int GRID_DIMENSION = 4;
+		final int TILE_HEIGHT = 200, TILE_WIDTH = 200;
+		final int SCREEN_WIDTH = 900, SCREEN_HEIGHT = 900;
+		
+		// Local variables
+		int cardNum = 1;
+		
+		// Root node for this JavaFX scene graph
+		BorderPane root = new BorderPane();
+		root.setPadding(new Insets(GAP, GAP, GAP, GAP));
+		
+		// TOP section of the BorderPane layout
+		VBox vbxTop = new VBox();
+		// Label for title
+		Label lblTitle = new Label("Cruise Route Random Selection");
+		lblTitle.setFont(Font.font(LARGE_FONT));
+		lblTitle.setAlignment(Pos.CENTER);
+		// Label for instructions
+		Label lblInstructions = new Label(RouteCard.showInstructions());
+		lblInstructions.setFont(Font.font(MEDIUM_FONT));
+		lblInstructions.setWrapText(true);
+		// Add to parent node VBox
+		vbxTop.getChildren().addAll(lblTitle, lblInstructions);
+		root.setTop(vbxTop);
+		BorderPane.setAlignment(vbxTop, Pos.TOP_CENTER);
+		
+		// CENTER section of the BorderPane layout
+		TilePane tileCenter = new TilePane(GAP, GAP);
+		tileCenter.setPrefColumns(GRID_DIMENSION);
+		tileCenter.setPrefTileHeight(TILE_HEIGHT);
+		tileCenter.setPrefTileWidth(TILE_WIDTH);
+		tileCenter.setPrefHeight(TILE_HEIGHT * GRID_DIMENSION);
+		tileCenter.setPrefWidth(TILE_WIDTH * GRID_DIMENSION);
+		
+		// Create 4x4 grid for RouteCards
+		RouteCard[][] routeGrid = new RouteCard[GRID_DIMENSION][GRID_DIMENSION];
+		
+		for (int row = 0; row < routeGrid.length; row++) {
+			for (int col = 0; col < routeGrid[row].length; col++) {
+				routeGrid[row][col] = new RouteCard(cardNum);	// instantiate each RouteCard in the 2D array
+				routeGrid[row][col].setOnAction(event -> selectRoute(event));
+				routeGrid[row][col].setPrefSize(TILE_WIDTH, TILE_HEIGHT);
+				tileCenter.getChildren().add(routeGrid[row][col]);
+				
+				cardNum++;
+			}
+		}
+		
+		root.setCenter(tileCenter);
+		
+		Scene routeScene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT);
+		
+		Window mainWindow = mainScene.getWindow();
+		
+		if (mainWindow instanceof Stage) {
+			Stage myStage = (Stage) mainWindow;
+			myStage.setTitle("Cruise Route Random Selection");
+			myStage.setScene(routeScene);
+			myStage.show();
+		}
+	}
+	
+	private void selectRoute(ActionEvent event) {
+		// The getSource() method returns the Object (RouteCard that was clicked) that generated this event
+		RouteCard temp = (RouteCard) event.getSource();	// cast to RouteCard object to access getValue() method from RouteCard class
 	}
 
 	public static void main(String[] args) {
