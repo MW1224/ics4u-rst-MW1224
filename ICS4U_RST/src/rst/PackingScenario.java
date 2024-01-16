522f6182cbfbdb20a2675abe6f0e4a5fb4416918
mayra <mayra@10.78.25.154>
2024-01-15 20:29:37 -0500

    No longer needed ITEMS as class constant -> local constant in getItems

--- ICS4U_RST/src/rst/PackingScenario.java
+++ ICS4U_RST/src/rst/PackingScenario.java
@@ -3,12 +3,6 @@ package rst;
 public class PackingScenario extends Scenario {
 	
 	public static final int WEIGHT_LIMIT = 40;
-	public static final String[] ITEMS = {"Jackets", "Shorts", "Jeans", "Pants", "Dresses", "Skirts", "Tops",
-			"Books", "Pairs of Socks", "Pyjama Sets", "Pairs of Shoes", "Scarves", "Hats", "Cell Phones",
-			"Empty Backpacks", "Makeup Products", "Lotion Tubes", "Bug Spray Bottles", "Hand Sanitizer Bottles",
-			"Bandaid Boxes", "Computers", "Tablets/iPads", "Portable Chargers", "Cameras", "Headphones",
-			"Earphones", "Sunglasses", "Travel Games", "Snorkels", "Watches", "Swimsuits", "Suit Sets", "Towels",
-			"Water Bottles", "Hair Accessories"};
 	
 	private Suitcase suitcase;
 	
@@ -17,11 +11,17 @@ public class PackingScenario extends Scenario {
 	}
 	
 	public static String[] getItems() {
-		String[] items = new String[ITEMS.length];
-		for (int i = 0; i < items.length; i++) {
-			items[i] = ITEMS[i] + " (" + Suitcase.WEIGHTS[i] + "):";
+		final String[] ITEMS = {"Jackets", "Shorts", "Jeans", "Pants", "Dresses", "Skirts", "Tops",
+				"Books", "Pairs of Socks", "Pyjama Sets", "Pairs of Shoes", "Scarves", "Hats", "Cell Phones",
+				"Empty Backpacks", "Makeup Products", "Lotion Tubes", "Bug Spray Bottles", "Hand Sanitizer Bottles",
+				"Bandaid Boxes", "Computers", "Tablets/iPads", "Portable Chargers", "Cameras", "Headphones",
+				"Earphones", "Sunglasses", "Travel Games", "Snorkels", "Watches", "Swimsuits", "Suit Sets", "Towels",
+				"Water Bottles", "Hair Accessories"};
+		String[] itemsWithWeights = new String[ITEMS.length];
+		for (int i = 0; i < itemsWithWeights.length; i++) {
+			itemsWithWeights[i] = ITEMS[i] + " (" + Suitcase.WEIGHTS[i] + "):";
 		}
-		return items;
+		return itemsWithWeights;
 	}
 	
 	public static String showInstructions() {
