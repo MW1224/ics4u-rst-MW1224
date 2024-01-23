ada2398977e0d83c2f0e525265c5684f18a81a4d
mayra <mayra@10.78.25.154>
2024-01-23 13:38:27 -0500

    Finished comments & revision for the FileHandler class

--- ICS4U_RST/src/rst/FileHandler.java
+++ ICS4U_RST/src/rst/FileHandler.java
@@ -13,27 +13,27 @@ import java.io.PrintWriter;
 public class FileHandler {
 	
 	/**
-	 * Loads information saved in the text file into an int that contains
+	 * This class method loads information saved in the text file into an int that contains
 	 * the high score out of all games and returns it.
 	 * 
 	 * @return An <code>int</code> containing the high score out of all
 	 * games from the text file.
 	 */
 	public static int loadFromFile() {
-		int highScore = 0;
+		int highScore = 0;	// initialize highScore variable to 0 as default
 		
 		try {	// handle exceptions that may occur when reading from file
 			// File handling objects
-			FileReader cruiseStoryGameFile = new FileReader("data/cruiseStoryGame.txt");
-			BufferedReader cruiseStoryGameStream = new BufferedReader(cruiseStoryGameFile);
+			FileReader cruiseStoryGameFile = new FileReader("data/cruiseStoryGame.txt");	// FileReader object reads file on hard drive
+			BufferedReader cruiseStoryGameStream = new BufferedReader(cruiseStoryGameFile);	// simplifies process of reading one line of data at a time as a String from file
 			
-			String line = cruiseStoryGameStream.readLine();
-			highScore = Integer.valueOf(line);
+			String line = cruiseStoryGameStream.readLine();	// read line from the file (for cruiseStoryGameFile: always only 1 line that contains high score)
+			highScore = Integer.valueOf(line);		// set highScore variable to numeric value of the high score obtained from the file
 			
 			cruiseStoryGameStream.close();	// close file
 		} catch (FileNotFoundException e) {
 			System.err.println("No file was found: " + e.getMessage());	// error message
-		} catch (IOException e) {
+		} catch (IOException e) {	// input/output exception
 			System.err.println("Problems reading the file: " + e.getMessage());	// error message
 		} catch (NumberFormatException e) {
 			System.err.println("File not formatted properly: " + e.getMessage());	// error message
@@ -43,10 +43,10 @@ public class FileHandler {
 	}
 	
 	/**
-	 * Saves high score to the text file.
+	 * This class method saves game's high score to the text file.
 	 *  
 	 * @param highScore
-	 * 			The high score of all games to be saved to text file.
+	 * 			The high score out of all games to be saved to text file.
 	 */
 	public static void saveToFile(int highScore) {
 		System.out.println("shutting down...");	// output message
@@ -55,9 +55,9 @@ public class FileHandler {
 			FileWriter cruiseStoryGameFile = new FileWriter("data/cruiseStoryGame.txt");	// creates new file on hard drive and writes data to it
 			PrintWriter cruiseStoryGamePrinter = new PrintWriter(cruiseStoryGameFile);		// simplifies process of writing different data types to file
 			
-			cruiseStoryGamePrinter.println(highScore);
-			cruiseStoryGamePrinter.close();	// close file
+			cruiseStoryGamePrinter.println(highScore);	// write high score to file
 			
+			cruiseStoryGamePrinter.close();	// close file
 		} catch (IOException e) {	// input/output exception
 			System.err.println("Error writing to file: " + e.getMessage());	// error message
 		}
